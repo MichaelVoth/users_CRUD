c50b240e21a75da925184b131da511dabbd6d1c3
Michael <michaeldvoth@me.com>
2023-04-04 09:56:27 -0700

    added comments

--- flask_app/controllers/user_routes.py
+++ flask_app/controllers/user_routes.py
@@ -3,34 +3,34 @@ from flask_app import app   #Imports flask app
 from flask_app.models.user import User #imports user class
 
 @app.route('/')
-def index_page():
+def index_page():           #displays all users in a table
     return render_template('index.html', users = User.all_users())
 
 @app.route('/add')
-def add_user():
+def add_user():             #redirects to add_user page
     return render_template('add_user.html')
 
 @app.route('/process',methods=['POST'])
-def process_new_user():
+def process_new_user():     #adds form data to DB and redirects to individual page
     user = User.create_user(request.form)
     return redirect(f'/show/{user}')
 
 @app.route('/show/<int:id>')
-def show_user(id):
+def show_user(id):          #Shows info for one user
     data = {'id':id}
     return render_template('show_user.html',user=User.show_user(data))
 
 @app.route('/edit/<int:id>')
-def edit_user(id):
+def edit_user(id):          #redirects to form with individual info displayed
     data = {'id':id}
     return render_template('edit_user.html',user = User.show_user(data))
 
 @app.route('/update', methods=['POST'])
-def update_user():
+def update_user():          #Updates user info in DB
     User.update_user(request.form)
     return redirect(f"/show/{request.form['id']}")
 
 @app.route('/delete/<int:id>')
-def delete(id):
+def delete(id):             #Removes user from DB
     User.delete(id)
     return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -11,7 +11,7 @@ class User:
         self.updated_at = data['updated_at']
 
     @classmethod
-    def all_users(cls):
+    def all_users(cls):             #Gets all info from DB
         query = "SELECT * FROM users;"
         results = connectToMySQL('users').query_db(query)
         users = []
@@ -20,27 +20,27 @@ class User:
         return users
     
     @classmethod
-    def create_user(cls, data):
+    def create_user(cls, data):     #Adds form data to DB as new row
         query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s,%(email)s);"
         results = connectToMySQL(cls.DB).query_db(query,data)
         return results
     
     @classmethod
-    def show_user(cls,data):
+    def show_user(cls,data):        #Gets one row of data from DB
         query = """SELECT * FROM users
                     WHERE id = %(id)s"""
         results = connectToMySQL(cls.DB).query_db(query, data)
         return cls(results[0])
     
     @classmethod
-    def update_user(cls,data):
+    def update_user(cls,data):      #Updates one row in DB
         query = """UPDATE users
                 SET first_name =%(first_name)s, last_name = %(last_name)s, email = %(email)s
                 WHERE id = %(id)s;"""
         return connectToMySQL(cls.DB).query_db(query,data)
 
     @classmethod
-    def delete(cls, id):
+    def delete(cls, id):            #Deletes one row from DB
         query  = "DELETE FROM users WHERE id = %(id)s;"
         data = {"id": id}
         return connectToMySQL(cls.DB).query_db(query, data) 
